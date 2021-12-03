9971fc5aab3953fb60fdc8f0865dc4e68934e7cb
michaelp3 <chan-park@hotmail.com>
2021-12-04 03:48:46 +0900

    final

--- src/chatting/ChatClient.java
+++ src/chatting/ChatClient.java
@@ -1,21 +1,38 @@
 package chatting;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.Socket; 
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.Scanner;
 
 public class ChatClient extends Thread {
 	static Scanner scan = new Scanner(System.in);
+	
+	Socket socket;
 	BufferedReader br;
+	InputStream is;
+	FileOutputStream fos;
+	BufferedOutputStream bos;
 	String id;
+	static int x = 0;
+	int bufferSize;
 	boolean exit = false;
 	
-	public ChatClient(BufferedReader br) {
+	public ChatClient(BufferedReader br, Socket client, String id) {
+		socket = client;
 		this.br = br;
+		this.id = id;
+		bufferSize = 0;
 	}
 	
 	public void run() {
@@ -23,11 +40,31 @@ public class ChatClient extends Thread {
 			if (br != null) {
 				try {
 					String line = br.readLine();
-					System.out.println(line);
+					if(line.equals("/f")) {
+						Socket nSocket = new Socket("127.0.0.1", 9999);
+						is = nSocket.getInputStream();
+			            bufferSize = nSocket.getReceiveBufferSize();
+			            fos = new FileOutputStream(id + "_test" + x + ".txt");
+			            bos = new BufferedOutputStream(fos);
+//						OutputStream out = new FileOutputStream(id + "_test" + x + ".txt");
+						x++;
+						byte[] bytes = new byte[bufferSize];
+
+				        int count;
+				        while ((count = is.read(bytes)) >= 0) {
+				            bos.write(bytes, 0, count);
+				        }
+				        bos.close();
+				        is.close();
+						nSocket.close();
+					}
+					else {
+						System.out.println(line);
+					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			} 
+			}
 		}
 	}
 	
@@ -45,28 +82,63 @@ public class ChatClient extends Thread {
 			pw.println(args[0]);
 			pw.flush();
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String inMessage = br.readLine();
-			System.out.println(inMessage);
+//			String inMessage = br.readLine();
+//			System.out.println(inMessage);
+			
+			
+//			OutputStream out = socket.getOutputStream();
+//			InputStream in = socket.getInputStream();
+			
+//			File file;
+//			long length = file.length();
+			
 			
 			//메세지 수신 대기
-			ChatClient client = new ChatClient(br);
+			ChatClient client = new ChatClient(br, socket, args[0]);
 			client.start();
 			
 			while(true) {
 				String line = scan.nextLine();
 				if(line.equals("quit")) {
-					pw.println(args[0] + "이 채팅방을 나갔습니다.");
+					pw.println(line);
 					pw.flush();
 					break;
 				}
-				System.out.println(line);
-				pw.println(args[0] + ": " + line);
-				pw.flush();
+				else if(line.length() > 2 && line.substring(0,2).equals("/f")) {
+					pw.println("/f");
+					pw.flush();
+					File file = new File(line.substring(3));
+					long length = file.length();
+					if(length > Long.MAX_VALUE) {
+						System.out.println("파일이 너무 큼.");
+					}
+					else {
+						Socket nSocket = new Socket("127.0.0.1", 12345);
+						InputStream fis = new FileInputStream(file);
+						BufferedInputStream bis = new BufferedInputStream(fis);
+						BufferedOutputStream out = new BufferedOutputStream(nSocket.getOutputStream());
+						
+						byte[] bytes = new byte[8192];
+						int count;
+						while ((count = bis.read(bytes)) > 0) {
+				            out.write(bytes, 0, count);
+				        }
+						out.close();
+						fis.close();
+						bis.close();
+					}
+				}
+				else {
+					pw.println(args[0] + ": " + line);
+					pw.flush();
+				}
 			}
 			client.end();
+//			if(in != null) {
+//				in.close();
+//			}
 			socket.close();
 		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
--- src/chatting/ChatServer.java
+++ src/chatting/ChatServer.java
@@ -1,9 +1,14 @@
 package chatting;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,14 +17,43 @@ import java.util.Hashtable;
 
 public class ChatServer extends Thread {
 	static Hashtable<String, PrintWriter> map = new Hashtable<String, PrintWriter>();
+	static Hashtable<String, Socket> map2 = new Hashtable<String, Socket>();
 	BufferedReader br;
 	String userId;
+	InputStream in;
 	
-	public ChatServer(String userId, BufferedReader br) throws IOException {
+	Socket socket;
+	InputStream is;
+	FileOutputStream fos;
+	BufferedOutputStream bos;
+	int bufferSize;
+	
+	static int x = 0;
+	
+	public ChatServer(String userId, BufferedReader br, PrintWriter pw, InputStream in, Socket socket) throws IOException {
 		this.userId = userId;
 		this.br = br;
+		this.in = in;
+		this.socket = socket;
 		
-		sendMessage(userId + "님이 입장 하였습니다.");
+		bufferSize = 0;
+		
+//		boolean exists = false;
+//		for(String key: map.keySet()) {
+//			if (key.equals(userId)) {
+//				exists = true;
+//				break;
+//			}
+//		}
+//		if(!exists) {
+//			map.put(userId, pw);
+//			sendMessage(userId + "님이 입장 하였습니다.");
+//		}
+		if(!map.containsKey(userId)){
+			sendMessage(userId + "님이 입장 하였습니다.");
+		}
+		map.put(userId, pw);
+		map2.put(userId, socket);
 	}
 	
 	void sendMessage(String line) throws IOException {
@@ -33,13 +67,78 @@ public class ChatServer extends Thread {
 		}
 	}
 	
+	void receiveFile() {
+		try {
+			ServerSocket nserverSocket = new ServerSocket(12345);
+			Socket nSocket = nserverSocket.accept();
+			is = nSocket.getInputStream();
+			bufferSize = nSocket.getReceiveBufferSize();
+			fos = new FileOutputStream("server_test" + x + ".txt");
+			x++;
+			bos = new BufferedOutputStream(fos);
+			byte[] bytes = new byte[bufferSize];
+			int count;
+            while ((count = is.read(bytes)) >= 0) {
+                bos.write(bytes, 0, count);
+            }
+            bos.close();
+            is.close();
+            nSocket.close();
+            nserverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	void sendFile() throws IOException {
+		Enumeration<String> keys = map.keys();
+		ServerSocket nServerSocket = new ServerSocket(9999);
+		while(keys.hasMoreElements()) {
+			String idKey = keys.nextElement();
+			PrintWriter pw = map.get(idKey);
+			pw.println("/f");
+			pw.flush();
+			Socket nSocket = nServerSocket.accept();
+			InputStream fis = new FileInputStream("server_test" + (x-1) + ".txt");
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			BufferedOutputStream out = new BufferedOutputStream(nSocket.getOutputStream());
+			byte[] bytes = new byte[8192];
+			int count;
+			while ((count = bis.read(bytes)) > 0) {
+	            out.write(bytes, 0, count);
+	        }
+			out.close();
+			fis.close();
+			bis.close();
+			nSocket.close();
+		}
+		nServerSocket.close();
+	}
+	
 	public void run() {
 		while(true) {
 			try {
 				String line = br.readLine();
-//				if(line != null) {
-					sendMessage(line);
-//				}
+				if(line != null) {
+					if(line.equals("quit")) {
+						line = userId + "이 채팅방을 나갔습니다.";
+						sendMessage(line);
+						map.remove(userId);
+						map2.remove(userId);
+					}
+					else if(line.equals("/f")) {
+						receiveFile();
+						sendFile();
+					}
+					else {
+						sendMessage(line);
+					}
+				}
+				else {
+					break;
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -51,6 +150,8 @@ public class ChatServer extends Thread {
 		// 서버 - ServerSocket -> accept -> Socket
 		BufferedReader br = null;
 		ServerSocket serverSocket = null;
+		InputStream in = null;
+		OutputStream out = null;
 		
 		try {
 			serverSocket = new ServerSocket(9990);
@@ -59,16 +160,18 @@ public class ChatServer extends Thread {
 				Socket socket = serverSocket.accept();
 				System.out.println("클라이언트 연결 됨.");
 				// 클라이언트 메세지 받기
+				in = socket.getInputStream();
+				out = socket.getOutputStream();
+//				br = new BufferedReader(new InputStreamReader(in));
 				br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				String userId = br.readLine();
 				System.out.println(userId + "님이 접속 됨.");
 				PrintWriter pw = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
 				
-				map.put(userId, pw);
-				
+//				map.put(userId, pw);
 				
 				// 브로드 케스트 준비
-				ChatServer server = new ChatServer(userId, br);
+				ChatServer server = new ChatServer(userId, br, pw, in, socket);
 				server.start();
 			}
 		} catch (IOException e) {
